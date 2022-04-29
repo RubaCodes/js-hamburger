95ed25ae2e50fa92c910763589f4a03fc8e00f25
rubacodes <rubacodes@gmail.com>
2022-04-29 14:50:59 +0200

    aggiunto listened ad open-menu

---
+++ main.js
@@ -0,0 +1,9 @@
+// al click dell'hamburger menu chiuso
+
+let button = document.getElementById('open-menu');
+
+button.addEventListener('click', 
+    function(){
+        alert("buton clicked!")
+    }
+);
