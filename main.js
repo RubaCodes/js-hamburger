cb47dda6a88daa831f42e60b5f9c07569c84b762
rubacodes <rubacodes@gmail.com>
2022-04-29 15:18:18 +0200

    implementata apertura del hamburger menu

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 
 let button = document.getElementById('open-menu');
 
-button.addEventListener('click', 
-    function(){
-        alert("buton clicked!")
+button.addEventListener('click',
+    function () {
+        let closedNav = document.getElementsByClassName('hamburger-menu');
+        closedNav.item(0).className = 'hamburger-menu active';
+        console.log(closedNav);
     }
 );
