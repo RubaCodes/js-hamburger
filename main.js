a1025b1d2b4c6a3c15ae3551e882679de029f160
rubacodes <rubacodes@gmail.com>
2022-04-29 16:37:04 +0200

    refactoring js

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 // IMPLEMENTAZIONE PURO JS SENZA TOCCARE HTML CON INDEX DELLA LISTA DI getElementsByClassName
 
-// al click dell'hamburger menu chiuso
 
+
+//hook ai buttons di apertura e chiusura
 const openMenu = document.getElementById('open-menu');
-// rendi block il tag hamburger menu
+const closeMenu = document.getElementById('close-menu');
+
+// al click dell'hamburger menu chiuso
+const hamburgerMenu = document.getElementsByClassName('hamburger-menu');
+
 openMenu.addEventListener('click',
     function () {
-        const closedNav = document.getElementsByClassName('hamburger-menu');
-        closedNav.item(0).className = 'hamburger-menu active';
-        console.log(closedNav);
+        // rendi block il tag hamburger menu
+        hamburgerMenu.item(0).className = 'hamburger-menu active';
+        console.log(' Hamburger Menu aperto');
     }
 );
 
+
 // al click dell'hamburger menu aperto
-const closeMenu = document.getElementById('close-menu');
-// rendi hidden il tag hamburger menu
 closeMenu.addEventListener('click',
     function () {
-        const closedNav = document.getElementsByClassName('hamburger-menu');
-        closedNav.item(0).className = 'hamburger-menu';
-        console.log(closedNav);
+        // rendi hidden il tag hamburger menu
+        hamburgerMenu.item(0).className = 'hamburger-menu';
+        console.log(' Hamburger Menu chiuso');
     }
 );
